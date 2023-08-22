c2cd0227cab8e4e5ce7bd6789a4f1180a4986770
fzdf0613 <fzdf0613@gmail.com>
2023-08-23 00:38:55 +0900

    CategoryItem 과 QuickLinkCard 에 Link 추가
    내용: 해당 컴포넌트 클릭 시 아이템 페이지로 이동하도록 링크 추가함

--- src/components/home/category/CategoryItem.tsx
+++ src/components/home/category/CategoryItem.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import Feature from "@/components/ui/Feature";
 import { Product } from "@/customType/product";
+import Link from "next/link";
 
 type Props = {
   product: Product;
@@ -8,18 +9,20 @@ type Props = {
 
 export default function CategoryItem({ product }: Props) {
   return (
-    <div className="mb-10 cursor-pointer">
-      <div className="w-full relative pb-[125%]">
-        <Image
-          className="object-cover"
-          src={product.imageUrl}
-          alt={product.name}
-          fill
-        />
+    <Link href={`items/${product.id}`}>
+      <div className="mb-10 cursor-pointer">
+        <div className="w-full relative pb-[125%]">
+          <Image
+            className="object-cover"
+            src={product.imageUrl}
+            alt={product.name}
+            fill
+          />
+        </div>
+        <h3 className="text-sm mt-2">{product.name}</h3>
+        <div className="text-[13px] text-[#6c6c6c] py-1">{`${product.price.toLocaleString()}원`}</div>
+        <Feature text={`${product.currentOrderCount}명이 주문중`} />
       </div>
-      <h3 className="text-sm mt-2">{product.name}</h3>
-      <div className="text-[13px] text-[#6c6c6c] py-1">{`${product.price.toLocaleString()}원`}</div>
-      <Feature text={`${product.currentOrderCount}명이 주문중`} />
-    </div>
+    </Link>
   );
 }
--- src/components/home/category/QuickLinkCard.tsx
+++ src/components/home/category/QuickLinkCard.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import Description from "@/components/ui/Description";
 import { Product } from "@/customType/product";
+import Link from "next/link";
 
 type Props = {
   product: Product;
@@ -8,18 +9,20 @@ type Props = {
 
 export default function QuickLinkCard({ product }: Props) {
   return (
-    <div className="px-4 pt-6 pb-12 bg-[#fafafa]">
-      <div className="relative pb-[61.2244%] mb-4">
-        <Image
-          className="object-cover"
-          priority
-          src={product.imageUrl}
-          alt="quick-link-image"
-          fill
-        />
+    <Link href={`/items/${product.id}`}>
+      <div className="px-4 pt-6 pb-12 bg-[#fafafa]">
+        <div className="relative pb-[61.2244%] mb-4">
+          <Image
+            className="object-cover"
+            priority
+            src={product.imageUrl}
+            alt="quick-link-image"
+            fill
+          />
+        </div>
+        <div className="text-xl font-bold">{product.homeTitle}</div>
+        <Description text={product.description} />
       </div>
-      <div className="text-xl font-bold">{product.homeTitle}</div>
-      <Description text={product.description} />
-    </div>
+    </Link>
   );
 }
