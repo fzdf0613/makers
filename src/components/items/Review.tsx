1234fef42543cd0c77eb44ae090569011d86a1bc
fzdf0613 <fzdf0613@gmail.com>
2023-09-03 19:17:42 +0900

    아이템 페이지 구매후기 등록 링크 추가

--- src/components/items/Review.tsx
+++ src/components/items/Review.tsx
@@ -1,5 +1,7 @@
+"use client";
 import { BiChevronRight } from "react-icons/bi";
 import ReviewList from "./ReviewList";
+import { useRouter } from "next/navigation";
 
 type Props = {
   reviewCount: number;
@@ -7,15 +9,21 @@ type Props = {
 };
 
 export default function Review({ reviewCount, postId }: Props) {
+  const router = useRouter();
   return (
     <div className="bg-white h-[1000px]">
-      <div className=" border-b border-[#f1f1f1] py-4">
-        <p className="text-[13px] text-center relative">
+      <div
+        className=" border-b border-[#f1f1f1] py-4 cursor-pointer"
+        onClick={() => {
+          router.push("/my/review?location=waiting");
+        }}
+      >
+        <div className="text-[13px] text-center relative">
           {"구매후기는 "}
           <span className="text-[#6c80e4] font-bold">후기내역 </span>
           에서 작성하실 수 있습니다.
           <BiChevronRight className="absolute right-5 top-0 w-5 h-5" />
-        </p>
+        </div>
       </div>
       <h4 className="font-bold py-4 text-sm mx-4">
         구매후기 {reviewCount.toLocaleString()}
