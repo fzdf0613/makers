3a8513176f04ac8f47825eeaff0e4611a4df52ca
fzdf0613 <fzdf0613@gmail.com>
2023-09-21 16:12:30 +0900

    my/like , my/reivew 페이지 비로그인 접근 제어 추가

--- src/app/(mainpages)/my/like/page.tsx
+++ src/app/(mainpages)/my/like/page.tsx
@@ -1,5 +1,14 @@
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
 import MyLikeList from "@/components/my/like/MyLikeList";
 
-export default function MyLikePage() {
+export default async function MyLikePage() {
+  const session = await getServerSession(authOptions);
+
+  if (!session) {
+    redirect(`/`);
+  }
+
   return <MyLikeList />;
 }
--- src/app/(mainpages)/my/review/page.tsx
+++ src/app/(mainpages)/my/review/page.tsx
@@ -1,11 +1,20 @@
 import MyReviewList from "@/components/my/review/MyReviewList";
 import ReviewTab from "@/components/my/review/ReviewTab";
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
 
-export default function MyReviewPage({
+export default async function MyReviewPage({
   searchParams,
 }: {
   searchParams: { location: string };
 }) {
+  const session = await getServerSession(authOptions);
+
+  if (!session) {
+    redirect(`/`);
+  }
+
   return (
     <>
       <ReviewTab tab={searchParams.location} />
