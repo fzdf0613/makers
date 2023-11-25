228c303c08931a1029ba584dc7472fa62d84f7b5
fzdf0613 <fzdf0613@gmail.com>
2023-11-25 22:12:14 +0900

    후기 내역 이미지 없을 때의 UI 수정

--- src/components/my/review/MyReviewItem.tsx
+++ src/components/my/review/MyReviewItem.tsx
@@ -31,17 +31,19 @@ export default function MyReviewItem({ review }: Props) {
       <div className="py-4">
         <pre className="text-sm whitespace-pre-wrap">{review.text}</pre>
       </div>
-      <button
-        onClick={() => {
-          setOpenImage((prev) => !prev);
-        }}
-        className={`h-[74px] w-[74px] relative`}
-      >
-        {openImage && (
-          <div className="absolute inset-0 border-[3px] border-[#4a4a4a] bg-[#0000004a] z-10" />
-        )}
-        <Image src={review.imageUrl} alt="review-thumbnail" fill />
-      </button>
+      {review.imageUrl && (
+        <button
+          onClick={() => {
+            setOpenImage((prev) => !prev);
+          }}
+          className={`h-[74px] w-[74px] relative`}
+        >
+          {openImage && (
+            <div className="absolute inset-0 border-[3px] border-[#4a4a4a] bg-[#0000004a] z-10" />
+          )}
+          <Image src={review.imageUrl} alt="review-thumbnail" fill />
+        </button>
+      )}
       {openImage && (
         <div className="w-full aspect-square bg-[#f1f1f1] relative mt-4">
           <Image
